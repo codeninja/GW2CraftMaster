8cdbe16de4d5dc0d498e5431d6e8f2716659239d
Dallas Pool <codeninja@gmail.com>
2013-01-07 16:16:10 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GW2CraftMaster::Application.config.secret_token = 'cdccc2f27e91b2835255f97e7a869af61987b2cdef0e874eb291251eff24c44ddbcb7eaeb930c743fea0a2a813722d61c5697f8da74d20055049ee8a827778d5'
